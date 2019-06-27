1299cf047202c18a8f8b801455abfe1df12b7b58
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-27 23:28:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,26 +15,4 @@ def printer(array)
   assign_rooms(array).each{|room| puts room}
 end
 
- # Para hacer que se apruebe esta prueba, asegúrese de estar iterando a través de sus insignias y listas de asignaciones de habitaciones.
- 
- #["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
- #                [
- #                "Hello, my name is Edsger.",
- #                "Hello, my name is Ada.",
- #                "Hello, my name is Charles.",
- #                "Hello, my name is Alan.",
- #                "Hello, my name is Grace.",
- #                "Hello, my name is Linus.",
- #                "Hello, my name is Matz."
- #                ]
- 
-#                         [
-#                            "Hello, Edsger! You'll be assigned to room 1!",
-#                            "Hello, Ada! You'll be assigned to room 2!",
-#                            "Hello, Charles! You'll be assigned to room 3!",
-#                            "Hello, Alan! You'll be assigned to room 4!",
-#                            "Hello, Grace! You'll be assigned to room 5!",
-#                            "Hello, Linus! You'll be assigned to room 6!",
-#                            "Hello, Matz! You'll be assigned to room 7!"
-#                         ]
- 
+# Para hacer que se apruebe esta prueba, asegúrese de estar iterando a través de sus insignias y listas de asignaciones de habitaciones.
