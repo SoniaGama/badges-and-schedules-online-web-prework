fb9e6bd43f928a7fc12de8cc1c3c01481163cbc3
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-27 22:52:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ end
 
 def batch_badge_creator(array)
   hello = ["Hello, my name is Edsger.", "Hello, my name is Ada.", "Hello, my name is Charles.", "Hello, my name is Alan.", "Hello, my name is Grace.", "Hello, my name is Linus.", "Hello, my name is Matz."]
-  array.each{|names| "Hello, my name is #{names}."}
+  array.each{|names| puts "Hello, my name is #{names}."}
 end
 
 def assign_rooms(array)
