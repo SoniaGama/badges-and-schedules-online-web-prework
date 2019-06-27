5d58941a48b1c9ebd49642401238d34037370096
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-27 23:20:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,17 +11,7 @@ def assign_rooms(array)
 end
 
 def printer
-  hello = ["Hello, my name is Edsger.", "Hello, my name is Ada.", "Hello, my name is Charles.", "Hello, my name is Alan.", "Hello, my name is Grace.", "Hello, my name is Linus.", "Hello, my name is Matz."]
-  
-  rooms = [
-    "Hello, Edsger! You'll be assigned to room 1!",
-    "Hello, Ada! You'll be assigned to room 2!",
-    "Hello, Charles! You'll be assigned to room 3!",
-    "Hello, Alan! You'll be assigned to room 4!",
-    "Hello, Grace! You'll be assigned to room 5!",
-    "Hello, Linus! You'll be assigned to room 6!",
-    "Hello, Matz! You'll be assigned to room 7!"
-  ]
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   batch_badge_creator(hello)
   assign_rooms(rooms)
 end
