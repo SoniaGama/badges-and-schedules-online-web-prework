303476b239c3c848e7e2d3e152b9e781adc80817
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-27 22:46:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,51 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each{|names| x}
+end
+
+def assign_rooms(array)
+  array.each{|names| x}
+end
+
+def printer
+  hello = ["Hello, my name is Edsger.", "Hello, my name is Ada.", "Hello, my name is Charles.", "Hello, my name is Alan.", "Hello, my name is Grace.", "Hello, my name is Linus.", "Hello, my name is Matz."]
+  
+  rooms = [
+    "Hello, Edsger! You'll be assigned to room 1!",
+    "Hello, Ada! You'll be assigned to room 2!",
+    "Hello, Charles! You'll be assigned to room 3!",
+    "Hello, Alan! You'll be assigned to room 4!",
+    "Hello, Grace! You'll be assigned to room 5!",
+    "Hello, Linus! You'll be assigned to room 6!",
+    "Hello, Matz! You'll be assigned to room 7!"
+  ]
+  batch_badge_creator(hello)
+  assign_rooms(rooms)
+end
+
+ # Para hacer que se apruebe esta prueba, asegúrese de estar iterando a través de sus insignias y listas de asignaciones de habitaciones.
+ 
+ #["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ #                [
+ #                "Hello, my name is Edsger.",
+ #                "Hello, my name is Ada.",
+ #                "Hello, my name is Charles.",
+ #                "Hello, my name is Alan.",
+ #                "Hello, my name is Grace.",
+ #                "Hello, my name is Linus.",
+ #                "Hello, my name is Matz."
+ #                ]
+ 
+#                         [
+#                            "Hello, Edsger! You'll be assigned to room 1!",
+#                            "Hello, Ada! You'll be assigned to room 2!",
+#                            "Hello, Charles! You'll be assigned to room 3!",
+#                            "Hello, Alan! You'll be assigned to room 4!",
+#                            "Hello, Grace! You'll be assigned to room 5!",
+#                            "Hello, Linus! You'll be assigned to room 6!",
+#                            "Hello, Matz! You'll be assigned to room 7!"
+#                         ]
+ 
--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -1,4 +1,5 @@
-require 'spec_helper'
+require_relative '../spec_helper.rb'
+#require '../conference_badges.rb'
 
 describe 'conference_badges' do
 
